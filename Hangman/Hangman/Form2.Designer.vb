e387ab0257036c1c9f0c1dfa3eb73771b7d6fda8
DAULL0002 <DAULL0002@kctcs.edu>
2016-11-19 02:23:27 -0600

    Players can now type in letters
    Letters appear on the screen to replace the "_" when the correct letter
    that goes there is typed in

--- Hangman/Hangman/Form2.Designer.vb
+++ Hangman/Hangman/Form2.Designer.vb
@@ -22,16 +22,30 @@ Partial Class frmGame
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(93, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(474, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Type in the letter you wish to check"
         '
         'frmGame
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(664, 319)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "frmGame"
         Me.Text = "Hangman"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
--- Hangman/Hangman/Form2.vb
+++ Hangman/Hangman/Form2.vb
@@ -1,19 +1,34 @@
 ﻿Public Class frmGame
 
     Dim phraseLength As Integer
+
+    Private Sub frmGame_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        Dim letterTyped As String
+        letterTyped = e.KeyChar
+        If Asc(letterTyped.ToUpper) > 64 And Asc(letterTyped.ToUpper) < 91 Then
+            If frmStartScreen.phrase.IndexOf(letterTyped.ToUpper) <> -1 Or frmStartScreen.phrase.IndexOf(letterTyped.ToLower) <> -1 Then
+                For x = 1 To phraseLength
+                    If frmStartScreen.phrase.Substring(x - 1, 1).ToUpper = letterTyped.ToUpper Then
+                        lblLetter(x).Text = frmStartScreen.phrase.Substring(x - 1, 1)
+                    End If
+                Next
+            End If
+        End If
+    End Sub
     Private Sub frmGame_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         
-
         newGame()
     End Sub
 
     Dim lblLetter() As Label
     Sub newGame()
         phraseLength = frmStartScreen.phrase.Length
-        Dim numberOfWords As Integer
+        Dim numberOfWords, wordCount As Integer
+        Dim secondLine As Boolean
         numberOfWords = frmStartScreen.phrase.Split(" ").Length
         Dim words(numberOfWords) As String
         words = frmStartScreen.phrase.Split(" "c)
+        wordCount = 1
         ReDim lblLetter(phraseLength)
         For x = 1 To phraseLength
             Dim letter As String
@@ -29,6 +44,7 @@
                 lblLetter(x).Text = "_"
             End If
 
+            
             If x = 1 Then
                 lblLetter(x).Left = 12
                 lblLetter(x).Top = 205
